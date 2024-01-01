8ef390c6c09c6647e723a439ce220da14f7e5f68
Efe Özdemir <ozdemir.efe.2001@gmail.com>
2024-01-02 01:28:08 +0300

    .

--- frontend/components/CardList.vue
+++ frontend/components/CardList.vue
@@ -5,5 +5,8 @@
 </template>
 
 <script setup>
-const { data: recipies } = await useFetch("http://localhost:5000/recipe/");
+const { data: recipies, pending, error, refresh } = await useAsyncData(
+  'recipie',
+  () => $fetch("http://localhost:5000/recipe/")
+)
 </script>
