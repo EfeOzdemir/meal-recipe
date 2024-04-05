9652e0764a0da79b62c00af2a99099cc5e622933
Efe Özdemir <ozdemir.efe.2001@gmail.com>
2024-04-05 17:52:28 +0300

    .

--- frontend/components/NewRecipeModal.vue
+++ frontend/components/NewRecipeModal.vue
@@ -17,6 +17,11 @@
           <Textarea class="h-[150px]" v-model="recipe.content" id="content" placeholder="Type your content here." />
         </div>
         <div class="grid w-full gap-1.5">
+          <Label for="ingredients">Ingredients</Label>
+          <Input id="ingredients" type="text" v-model="recipe.ingredients" placeholder="Ingredient1, Ingredient2, ..." />
+        </div>
+
+        <div class="grid w-full gap-1.5">
           <Label for="content">Category</Label>
           <Select v-model="recipe.category_id">
             <SelectTrigger>
@@ -43,50 +48,42 @@
   </DialogRoot>
 </template>
 <script setup>
-import { Textarea } from "@/components/ui/textarea";
-import { DialogRoot } from "radix-vue";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { Textarea } from '@/components/ui/textarea';
+import { DialogRoot } from 'radix-vue';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 const store = useUserStore();
 const show = ref(false);
 const recipe = ref({});
 const categories = ref([]);
-const file = ref(null)
+const file = ref(null);
 
 const getCategories = async () => {
-  const category = await $fetch("https://hopeful-vim-417109.oa.r.appspot.com/recipe/category");
+  const category = await $fetch('https://hopeful-vim-417109.oa.r.appspot.com/recipe/category');
   categories.value = category;
 };
 
 const handleFileChange = (event) => {
-  file.value = event.target.files[0]
-}
+  file.value = event.target.files[0];
+};
 
 const add = async () => {
   if (!recipe.value.title || !recipe.value.content || !recipe.value.category_id || !file.value) {
-    alert("Fill empty fields.")
-    return
+    alert('Fill empty fields.');
+    return;
   }
 
   const formData = new FormData();
   formData.append('title', recipe.value.title);
   formData.append('content', recipe.value.content);
   formData.append('category_id', recipe.value.category_id);
-  formData.append('ingredients', ["su"]);
+  formData.append('ingredients', recipe.value.ingredients);
   formData.append('image', file.value);
 
-
-  await $fetch("https://hopeful-vim-417109.oa.r.appspot.com/recipe/", {
-    method: "post",
-    headers: { authorization: "Bearer " + store.userToken },
-    body: formData
+  await $fetch('https://hopeful-vim-417109.oa.r.appspot.com/recipe/', {
+    method: 'post',
+    headers: { authorization: 'Bearer ' + store.userToken },
+    body: formData,
   });
   show.value = false;
   window.location.reload();
